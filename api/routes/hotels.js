9b45dacc7248727804041bf70e8a753a8649462e
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-10 23:31:58 -0800

    added hotel delete route

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -5,6 +5,7 @@ const router = express.Router()
 
 // CREATE
 router.post("/", async (req, res) => {
+
     const newHotel = new Hotel(req.body);
 
     try{
@@ -13,9 +14,28 @@ router.post("/", async (req, res) => {
     }catch(err){
         res.status(500).json(err)
     }
+
 })
 // UPDATE
+router.put("/:id", async (req, res) => {
+    try{
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, {new: true}) 
+        res.status(200).json(updatedHotel)
+    }catch(err){
+        res.status(500).json(err)
+    }
+    
+})
 // DELETE
+router.delete("/:id", async (req, res) => {
+    try{
+        const updatedHotel = await Hotel.findByIdAndDelete(req.params.id) 
+        res.status(200).json("Hotel deleted successfuly")
+    }catch(err){
+        res.status(500).json(err)
+    }
+    
+})
 // GET
 // GET ALL
 
