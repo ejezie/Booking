3eb0e39f1a8c6ba6ca5eb4a8e3535b01fddf25a5
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-13 14:27:17 -0800

    adding user model

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,9 +2,6 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send("auth!")
-})
 router.get("/register", (req, res) => {
     res.send("auth register to it")
 })
