815b854becd9bd419ca6d57eed449432cc61edd9
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-14 00:46:48 -0800

    hash password

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,11 +1,16 @@
 import User from "../models/User.js"
+import bycrpt from "bcryptjs"
 
 export const register = async (req, res, next) => {
     try{
+
+        const salt = bycrpt.genSaltSync(10);
+        const hash = bycrpt.hashSync(req.body.password, salt)
+
         const newUser = new User({
             username: req.body.username,
             password: req.body.password,
-            email: req.body.email,
+            email: hash,
         })
 
         await newUser.save();
