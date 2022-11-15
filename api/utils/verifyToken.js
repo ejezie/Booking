a804a61343369967fe84c7b228ccc01d8dd23c95
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-15 15:58:59 -0800

    Update verifyToken.js

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -4,7 +4,7 @@ import { makeError } from "./error.js";
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
 
-    if(!token) (next(makeError(401, 'Sorry, ou are not authenticated')))
+    if(!token) (next(makeError(401, 'Sorry, you are not authenticated')))
 
     jwt.verify(token, process.env.JWT, (err, user) => {
 
