44232208d2f84e0ff47ee1a7859684ecfee33f68
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-15 13:49:44 -0800

    update, get, delete user functions

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -8,3 +8,42 @@ export const getAllUsers = async (req, res, next) =>  {
         next(err);
     }
 }
+
+import Hotel from "../models/Hotel.js";
+
+export const createUser = async (req, res, next) => {
+    const newHotel = new User(req.body);
+    try{
+        const savedHotel = await newHotel.save() ;
+        res.status(200).json(savedHotel);
+    }catch(err){
+        next(err);
+    }
+}
+
+export const updateUser = async (req, res, next) => {
+     try{
+        const updatedHotel = await User.findByIdAndUpdate(req.params.id, { $set: req.body }, {new: true}) 
+        res.status(200).json(updatedHotel)
+    }catch(err){
+        next(err)
+    }
+}
+
+export const getUserById = async (req, res, next) => {
+     try{
+        const hotel = await User.findById(req.params.id) 
+        res.status(200).json(hotel)
+    }catch(err){
+        next()
+    }
+}
+
+export const deleteUser = async (req, res, next) => {
+    try{
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("Hotel deleted succesfully")
+    }catch(err){
+        next(err)
+    }
+}
