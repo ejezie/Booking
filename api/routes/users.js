f95bbc25e7866530381e69f6599352d639cff1d1
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-15 13:58:18 -0800

    update user router

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { getAllUsers } from "../controllers/user.js";
+import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from "../controllers/user.js";
 
 const router = express.Router()
 
 router.get('/', getAllUsers)
+
+router.post('/', createUser)
+
+router.delete('/:id', deleteUser)
+
+router.get('/:id', getUserById)
+
+router.put('/id', updateUser)
 
 export default router;
