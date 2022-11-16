ad69cf0c18160b25dbaddfc7dd32e317a19d8abe
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-15 23:25:17 -0800

    Create room controller function

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -1,10 +1,18 @@
-import Room from "../models/Room";
-import { makeError } from "../utils/error";
+import Room from "../models/Room.js";
+import Hotel from "../models/Hotel.js";
+import { makeError } from "../utils/error.js";
 
 const createRoom = async (req, res, next) => {
+  const hotelId = req.params.hotellId;
+  const newRoom = new Room(req.body);
+  try {
+    const savedRoom = await newRoom.save();
     try{
-        const room = new Room(req.body);
+        await Hotel.findByIdAndUpdate(hotelId, {$push: {rooms: savedRoom._id}})
     }catch(err){
         next(err)
     }
-}
+  } catch (err) {
+    next(err);
+  }
+};
