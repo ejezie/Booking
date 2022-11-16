a6bf2bbe49cde017c3066a4923889c7465cc8da0
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-15 16:43:20 -0800

    fix err not defined

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -18,7 +18,7 @@ export const verifyUser = (req, res, next) => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next();
     } else {
-      if (err) next(makeError(403, "Invalid Token"));
+      return next(makeError(403, "You are not authorised"));
     }
   });
 };
