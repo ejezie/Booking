3e9c40adc9213252cfe5669baef364fac55a8152
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-16 00:01:58 -0800

    Update room routers

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { getAllRooms } from "../controllers/room";
-import { verifyAdmin } from "../utils/verifyToken";
+import { createRoom, deleteRoom, getAllRooms, getRoomById, updateRoom } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router()
 
-router.get('/', verifyAdmin, getAllRooms)
+router.get('/', getAllRooms );
+
+router.get('/:id',getRoomById )
+
+router.delete('/:id', verifyAdmin, deleteRoom)
+
+router.put('/:id', verifyAdmin, updateRoom)
+
+router.post('/:id', verifyAdmin,createRoom)
 
 
 export default router;
