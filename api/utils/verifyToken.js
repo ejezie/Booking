08164a873193023f365613fb838cdd90c7d1957b
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-15 16:21:34 -0800

    verify user before delete auth

--- api/utils/verifyToken.js
+++ api/utils/verifyToken.js
@@ -2,16 +2,23 @@ import jwt from "jsonwebtoken";
 import { makeError } from "./error.js";
 
 export const verifyToken = (req, res, next) => {
-    const token = req.cookies.access_token;
+  const token = req.cookies.access_token;
 
-    if(!token) (next(makeError(401, 'Sorry, you are not authenticated')))
+  if (!token) next(makeError(401, "Sorry, you are not authenticated"));
 
-    jwt.verify(token, process.env.JWT, (err, user) => {
+  jwt.verify(token, process.env.JWT, (err, user) => {
+    if (err) next(makeError(403, "Invalid Token"));
+    req.user = user;
+    next();
+  });
+};
 
-        if(err) (next(makeError(403, 'Invalid Token')));
-        req.user = user;
-        next();
-
-    })
-    
-}
+export const verifyUser = (req, res, next) => {
+  verifyToken(req, res, next, () => {
+    if (req.param.id === user.id) {
+      next();
+    } else {
+      if (err) next(makeError(403, "Invalid Token"));
+    }
+  });
+};
