a688861053e5d9bba97c992b077d48d85d016020
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-15 23:58:59 -0800

    set room route

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,5 +1,10 @@
 import express from "express";
+import { getAllRooms } from "../controllers/room";
+import { verifyAdmin } from "../utils/verifyToken";
 
 const router = express.Router()
+
+router.get('/', verifyAdmin, getAllRooms)
+
 
 export default router;
