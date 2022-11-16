1dfda44209bc31468d85afba6520b55b56a68c65
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-15 19:04:24 -0800

    adding room models

---
+++ api/models/Room.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+
+const {Schema, model} = mongoose;
+
+const RoomSchema = Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    price : {
+        type: Number ,
+        required: true,
+    }, 
+    desc: {
+        type: String,
+        required: true,
+    },
+    maxPeople: {
+        type: Number,
+        required: true,
+    },
+    roomNumber: [{}],
+    photos: {
+        type: [String],
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        min: 0,
+        max: 5
+    },
+    room: {
+        type: String,
+    },
+    cheapestPrice: {
+        type: Number,
+        required: true,
+    },
+    featured: {
+        type: Boolean,
+        default: false,
+    }
+})
+
+export default model("Room", RoomSchema);
