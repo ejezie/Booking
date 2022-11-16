85bda0ffad6678f4d3a6d7f24cedad9644b348f3
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-15 23:40:59 -0800

    get all rooms

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -36,3 +36,11 @@ const getRoomById = async (req, res, next) => {
     }
 }
 
+const getAllRooms = async (req, res, next) => {
+    try{
+        const rooms = await Room.find()
+        res.status(200).json(rooms)
+    }catch(err){
+        next(err);
+    }
+}
