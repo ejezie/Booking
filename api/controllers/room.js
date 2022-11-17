bec572fd09d4f44eb36b679937a9e26c20c3e302
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-16 23:15:04 -0800

    Update room.js

--- api/controllers/room.js
+++ api/controllers/room.js
@@ -7,10 +7,12 @@ export const createRoom = async (req, res, next) => {
   const newRoom = new Room(req.body);
   try {
     const savedRoom = await newRoom.save();
-    try{
-        await Hotel.findByIdAndUpdate(hotelId, {$push: {rooms: savedRoom._id}})
-    }catch(err){
-        next(err)
+    try {
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $push: { rooms: savedRoom._id },
+      });
+    } catch (err) {
+      next(err);
     }
     res.status(200).json(savedRoom);
   } catch (err) {
@@ -19,39 +21,50 @@ export const createRoom = async (req, res, next) => {
 };
 
 export const updateRoom = async (req, res, next) => {
-    try{
-        const updatedRoom = await Room.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true})
-        res.status(200).json(updateRoom)
-    }catch(err){
-        next(err)
-    }
-}
+  try {
+    const updatedRoom = await Room.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updateRoom);
+  } catch (err) {
+    next(err);
+  }
+};
 
 export const getRoomById = async (req, res, next) => {
-    try{
-        const room = await Room.findById(req.params.id);
-        res.status(200).json(room)
-    }catch(err){
-        next(err);
-    }
-}
+  try {
+    const room = await Room.findById(req.params.id);
+    res.status(200).json(room);
+  } catch (err) {
+    next(err);
+  }
+};
 
 export const getAllRooms = async (req, res, next) => {
-    try{
-        const rooms = await Room.find()
-        res.status(200).json(rooms)
-    }catch(err){
-        next(err);
-    }
-}
+  try {
+    const rooms = await Room.find();
+    res.status(200).json(rooms);
+  } catch (err) {
+    next(err);
+  }
+};
 
 export const deleteRoom = async (req, res, next) => {
-    const hotelId = req.params.hotelId;
-    try{
-        await Room.findByIdAndDelete(req.params.id)
-        await Hotel.findByIdAndUpdate(hotelId, {$pull: {rooms: req.params.id}})
-        res.status(200).send("Room deleted successfully")
-    }catch(err){
-        next(err)
+  const hotelId = req.params.hotelId;
+  try {
+    await Room.findByIdAndDelete(req.params.id);
+    try {
+      await Hotel.findByIdAndUpdate(hotelId, {
+        $push: { rooms: savedRoom._id },
+      });
+    } catch (err) {
+      next(err);
     }
-}
+    ß;
+    res.status(200).send("Room deleted successfully");
+  } catch (err) {
+    next(err);
+  }
+};
