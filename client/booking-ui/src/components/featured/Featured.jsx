5f3081193741df081a59781a79ce94645e3b4c87
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-18 18:27:09 -0800

    Update Featured.jsx

--- client/booking-ui/src/components/featured/Featured.jsx
+++ client/booking-ui/src/components/featured/Featured.jsx
@@ -22,7 +22,7 @@ const Featured = () => {
               className="featuredImg"
             />
             <div className="featuredTitles">
-              <h1>Calaba</h1>
+              <h1>Lagos</h1>
               <h2>{data[2]} properties</h2>
             </div>
           </div>
@@ -34,7 +34,7 @@ const Featured = () => {
             />
             <div className="featuredTitles">
               <h1>Abuja</h1>
-              <h2>{data[1]} properties</h2>
+              <h2>{data[0]} properties</h2>
             </div>
           </div>
           <div className="featuredItem">
@@ -44,8 +44,8 @@ const Featured = () => {
               className="featuredImg"
             />
             <div className="featuredTitles">
-              <h1>Lagos</h1>
-              <h2>{data[0]} properties</h2>
+              <h1>Calaba</h1>
+              <h2>{data[2]} properties</h2>
             </div>
           </div>
         </>
