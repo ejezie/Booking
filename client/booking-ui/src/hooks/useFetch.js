31e7c1384083c0024ffbdee0759e67b8b3173b5d
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-18 16:11:02 -0800

    refetch data func

--- client/booking-ui/src/hooks/useFetch.js
+++ client/booking-ui/src/hooks/useFetch.js
@@ -2,21 +2,32 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const useFetch = (url) => {
-    const [data, setData] = useState([])
-    const [loadiing, setLoadiing] = useState(false)
-    const [error, setError] = useState(false)
+  const [data, setData] = useState([]);
+  const [loadiing, setLoadiing] = useState(false);
+  const [error, setError] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoadiing(true);
-            try{
-                const res = await axios.get(url);
-                data && setData(res.data)
-            }catch(err){
-                setError(err)
-            }
-            setLoadiing(false)
-        }
-        fetchData()
-    }, [url])
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoadiing(true);
+      try {
+        const res = await axios.get(url);
+        data && setData(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoadiing(false);
+    };
+    fetchData();
+  }, [url]);
+
+  const reFetchData = async () => {
+    setLoadiing(true);
+    try {
+      const res = await axios.get(url);
+      data && setData(res.data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoadiing(false);
+  };
+};
