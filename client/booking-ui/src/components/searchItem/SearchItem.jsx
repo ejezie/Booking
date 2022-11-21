a81d9fee5a05c600863ba616b62489743fd152c5
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-21 11:23:06 -0800

    Update SearchItem.jsx

--- client/booking-ui/src/components/searchItem/SearchItem.jsx
+++ client/booking-ui/src/components/searchItem/SearchItem.jsx
@@ -9,8 +9,8 @@ const SearchItem = ({item}) => {
         className="siImg"
       />
       <div className="siDesc">
-        <h1 className="siTitle">Tower Street Apartments</h1>
-        <span className="siDistance">500m from center</span>
+        <h1 className="siTitle">{item.name}</h1>
+        <span className="siDistance">{item.distance} from center</span>
         <span className="siTaxiOp">Free airport taxi</span>
         <span className="siSubtitle">
           Studio Apartment with Air conditioning
