7d02dc3d3429ac46b1678867db5251e9edc288a9
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-20 16:44:11 -0800

    Update FeaturedProperties.jsx

--- client/booking-ui/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/booking-ui/src/components/featuredProperties/FeaturedProperties.jsx
@@ -11,6 +11,8 @@ const FeaturedProperties = () => {
     "https://cf.bstatic.com/xdata/images/hotel/max1280x900/322658536.jpg?k=3fffe63a365fd0ccdc59210188e55188cdb7448b9ec1ddb71b0843172138ec07&o=&hp=1",
   ];
 
+  console.log(data, "****");
+
   return (
     <div className="fp">
       {loading ? (
