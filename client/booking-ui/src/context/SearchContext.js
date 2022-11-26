66edeb506e21a32cda919fd24a5e55a0984f8718
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-11-25 23:50:12 -0800

    Update SearchContext.js

--- client/booking-ui/src/context/SearchContext.js
+++ client/booking-ui/src/context/SearchContext.js
@@ -18,5 +18,7 @@ const searchReducer = (state, action) => {
           return action.payload
         case "RESET_SEARCH":
           return INITIAL_STATE;
+        default:
+          return state;
     }
 }
