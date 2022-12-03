831989b428dfe8bdd75cb35227f48dddeb2a9aee
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-12-03 15:53:04 -0800

    Update Login.jsx

--- client/booking-ui/src/pages/login/Login.jsx
+++ client/booking-ui/src/pages/login/Login.jsx
@@ -1,15 +1,29 @@
-import React, {useState} from 'react'
-import "./login.css"
+import React, { useState } from "react";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import "./login.css";
 
 function Login() {
+  const [credential, setCredential] = useState({
+    username: undefined,
+    password: undefined,
+  });
 
-    const [credential, setCredential] = useState({
-        username: undefined,
-        password: undefined,
-    })
-  return (
-    <div>Login</div>
-  )
+  const handleChange = () => {
+
+  }
+  const handleClick = () => {
+
+  }
+
+  const {loading, error, dispatch} = useContext(AuthContext)
+  return <div className="login">
+    <div className="loginWrap">
+        <input placeholder="user name" id="username" type={"text"} onChange={handleChange} className="loginInput"/>
+        <input placeholder="user name" id="password " type={"password"} onChange={handleChange} className="login Input"/>
+        <button className="loginBut" onClick={handleClick}>Login</button>
+    </div>
+  </div>;
 }
 
-export default Login
+export default Login;
