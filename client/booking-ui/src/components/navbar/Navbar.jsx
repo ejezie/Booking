72da575aa27e623aa5270e6b1702140b9ddb6e06
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2022-12-04 08:55:08 -0800

    hide/show login/register btn based on user existence

--- client/booking-ui/src/components/navbar/Navbar.jsx
+++ client/booking-ui/src/components/navbar/Navbar.jsx
@@ -1,17 +1,22 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 import "./navbar.css";
 
 const Navbar = () => {
+
+  const {user} = useContext(AuthContext)
+
   return (
     <div className="navbar">
       <div className="navContainer">
         <Link to={"/"} style={{color: 'white', textDecoration: 'none'}}>
           <span className="logo">BookNow</span>
         </Link>
-        <div className="navItems">
+       {!user && <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
-        </div>
+        </div>}
       </div>
     </div>
   );
